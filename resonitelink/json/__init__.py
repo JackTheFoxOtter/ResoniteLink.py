e14229e927625340772a543212fb3ed73e6dbb68
JackTheFoxOtter <jackthefoxotter@gmail.com>
2026-01-10 19:50:38 +0100

    Reduced which types are imported in json module.

--- resonitelink/json/__init__.py
+++ resonitelink/json/__init__.py
@@ -1,3 +1,3 @@
-from .decoder import *
-from .encoder import *
-from .models import *
+from .decoder import JSONDecoder
+from .encoder import JSONEncoder
+from .models import JSONModel, JSONProperty, json_model, get_model_for_data_class, get_model_for_type_name
